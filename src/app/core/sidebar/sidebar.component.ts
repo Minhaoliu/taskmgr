d30ff7d5e04be84946db36a0208f603efa8d3444
Minhaoliu <s1252307@monmouth.edu>
2020-08-30 00:44:23 -0400

    sidebar

--- src/app/core/sidebar/sidebar.component.ts
+++ src/app/core/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { getDate } from 'date-fns';
+
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  today = 'day';
+
   constructor() { }
 
   ngOnInit() {
+    this.today = `day${getDate(new Date())}`;
   }
 
 }
