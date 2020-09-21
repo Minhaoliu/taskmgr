af7adf9147e521e80471eda934431eaf4571373e
Minhaoliu <s1252307@monmouth.edu>
2020-09-21 18:33:24 -0400

    animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,15 @@
 import { OverlayContainer } from '@angular/material';
 import { Component } from '@angular/core';
-import { trigger, state, transition, style, animate } from '@angular/animations';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  animations: [
-    trigger('square',
-      [
-        state('green', style({ 'background-color': 'green', 'height': '100px', 'transform': 'translateX(0)' })),
-        state('red', style({ 'background-color': 'red', 'height': '50px', 'transform': 'translateX(100%)' })),
-        transition('green => red', animate('.2s 1s')),
-        transition('red => green', animate(1000))
-      ]
-    )
-  ]
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
-  squareState: string;
+
   darkTheme = false;
 
   constructor(private oc: OverlayContainer) {
@@ -29,9 +19,5 @@ export class AppComponent {
   switchTheme(dark) {
     this.darkTheme = dark;
     this.oc.themeClass = dark ? 'myapp-dark-theme' : null;
-  }
-
-  onClick() {
-    this.squareState = this.squareState === 'red' ? 'green' : 'red';
   }
 }
--- src/app/task/task-item/task-item.component.ts
+++ src/app/task/task-item/task-item.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { itemAnim } from './../../anims/item.anim';
+import { Component, OnInit, Input, Output, EventEmitter, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
-  styleUrls: ['./task-item.component.scss']
+  styleUrls: ['./task-item.component.scss'],
+  animations: [
+    itemAnim
+  ]
 })
 export class TaskItemComponent implements OnInit {
 
   @Input() item;
   @Input() avatar;
   @Output() taskClick = new EventEmitter<void>();
+  widerPriority = 'in';
 
   constructor() { }
 
@@ -24,4 +29,15 @@ export class TaskItemComponent implements OnInit {
   onCheckBoxClick(ev: Event) {
     ev.stopPropagation();
   }
+
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    this.widerPriority = 'out';
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    this.widerPriority = 'in';
+  }
+
 }
